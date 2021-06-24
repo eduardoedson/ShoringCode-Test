a398dc0016d1f5c1f0f09a22a0092f1d32c9790f
Eduardo Edson Batista Cordeiro Alves <edu-23@hotmail.com>
2021-06-24 18:54:05 -0300

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+	return (
+		<footer className="footer">Eduwo Tasks @ 2021</footer>
+	);
+}
+
+export default Footer;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import '../assets/css/Global.css'
 
 const Index = () => {
 	return (
 		<div className="App">
 			<Header />
+			<Footer />
 		</div>
 	);
 }
